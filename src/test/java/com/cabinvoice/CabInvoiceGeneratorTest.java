a63503196165d3e3513e1da0b0eca7bbd9de9959
unknown <himanshu9518@gmail.com>
2023-07-10 13:07:51 +0530

    Second commit

--- src/test/java/com/cabinvoice/CabInvoiceGeneratorTest.java
+++ src/test/java/com/cabinvoice/CabInvoiceGeneratorTest.java
@@ -1,17 +1,18 @@
 package com.cabinvoice;
 
+
 import org.junit.Assert;
 import org.junit.Test;
 
-class CabInvoiceGeneratorTest {
-CabInvoiceGenerator obj=new CabInvoiceGenerator();
+public class CabInvoiceGeneratorTest {
+    CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+    @Test
+    public  void TotalFare(){
+        double distance = 2.0;
+        int time = 5;
+        double result = cabInvoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25,result,0);
 
-@Test
-    public void totalFare(){
-    double distance=2.0;
-    int time=5;
-    double totalFare= obj.calculateFare(distance,time);
-    Assert.assertEquals(25,totalFare);
+    }
 
-   }
 }
